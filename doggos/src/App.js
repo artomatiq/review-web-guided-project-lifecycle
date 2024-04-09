edf02798e2dc5ec17c4a1fa9e0e4f0238f255cb3
Art Vardanyan <akvardanyan99@gmail.com>
2024-04-09 07:25:41 -0400

    initial

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,2 +1,30 @@
 import React from 'react';
 import axios from 'axios';
+
+
+export default class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {dogs: []}
+    }
+
+    componentDidMount() {
+        axios.get('dog.ceo/api/breeds/image/random')
+            .then (res => {
+                console.log(res.data)
+                this.setState({dogs: res.data})
+            })
+            .catch(err => {
+                alert('Error fetching dogs', err.message)
+            })
+    }
+
+    render() {
+        return (
+            <div className='App container' >
+                hello
+            </div>
+        );
+    }
+}
